ed614d128661e9f2d2cdf83faa23b8897d32bacb
Alex Paydarfar <Alex Paydarfar>
2017-07-25 08:45:37 -0700

    Multiple files updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(function () {
 
   var url = 'https://api.nytimes.com/svc/topstories/v2/';
-  var selectionName = $('.selection').val();
+  var selectionName = $('.selection').val()
+  var $container = $('.container')
 
   $('.selection').on('change', function () {
     event.preventDefault();
@@ -26,26 +27,25 @@ $(function () {
       var i = 0;
       var results = data.results;
       var x = [];
-      console.log(data);
 
       $.each(results, function (key, value) {
         var storyAbstract = value.abstract;
         console.log(storyAbstract);
         var storyUrl = value.url;
-        var storyPhoto = results[0].multimedia[4].url;
-        var arrayPhoto = value.multimedia;
-        if (arrayPhoto.length) {
-          x.push(data.results[i])
-          results = x.slice(0, 12);
-          i++;
-        } else {
-          i++;
-        }
+        var storyPhoto = value.multimedia[4].url;
+        // var arrayPhoto = value.multimedia;
+        // if (arrayPhoto.length) {
+        //   x.push(data.results[i])
+        //   results = x.slice(0, 12);
+        //   i++;
+        // } else {
+        //   i++;
+        // }
         console.log(results, x);
         var style = 'background-image:url(' + storyPhoto + ');';
         console.log(style);
         var storyListItem = '<li class="story-items"><a href=' + storyUrl + ' target="_blank">';
-        storyListItem += '<div class="photo" style="height: 200px; ' + style + '"><p class="list-text">';
+        storyListItem += '<div class="photo" style="height: 400px; ' + style + '"><p class="list-text">';
         storyListItem += storyAbstract;
         storyListItem += '</p></div></a></li>';
         $('.article-list').append(storyListItem);
